c7c24b02c1e49f12a33e9fafbc35a4b8b9fedf40
AnaBVA <anabva2304@gmail.com>
2021-04-08 15:23:50 -0500

    adding ex 2,3 from python village and ex 1 from bioinformatics

---
+++ rosalind/Python_Village/Variables_and_Some_Arithmetic.py
@@ -0,0 +1,7 @@
+# Variables
+a = 828
+b = 808
+
+# Calculate the hypotenuse
+hypotenuse = a ** 2 + b ** 2
+print(hypotenuse)
---
+++ rosalind/Python_Village/strings_and_lists.py
@@ -0,0 +1,11 @@
+## String
+s = "D9ENZbamYRxjC2UNpk9rJX4oa2LPvkQN9zFwvvnV4mdSuRrcOQQsTDI6UhbBjParus5Y5HDO5DAH9Kcristatellus3uXiPhIukrilxYdixBnADTN23zQSMR7Qrhc7JuaXosmukqjt5klBMEt5Cmth5QvGK6JaESSEqOf5EkMmp0."
+
+## Variables
+a = 61
+b = 65
+c = 78
+d = 89
+
+## Output
+print(s[a:b+1],s[c:d+1])
